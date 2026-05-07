5f90875396e58e90e2c974ac6f2457faf566f12c
Omar <robodude20000@gmail.com>
2026-05-07 04:19:50 +0100

    Second commit

--- rover-server/main.py
+++ rover-server/main.py
@@ -27,6 +27,15 @@ config = ServerConfig()
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading', cors_allowed_origins='*')
 
+
+@app.after_request
+def add_cors_headers(response):
+    """Add CORS headers to all responses."""
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
 # Initialize hardware interfaces
 rover_controller = RoverController(
     bus=config.spi_bus,
